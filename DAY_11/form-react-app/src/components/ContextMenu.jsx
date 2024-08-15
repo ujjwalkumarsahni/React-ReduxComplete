462815bb9eb54de92aed052bd0e7e3052a9ea8c0
Ujjwal Kumar <133194307+ujjwalkumarsahni@users.noreply.github.com>
2024-08-15 23:35:44 +0530

    DAY_11

---
+++ DAY_11/form-react-app/src/components/ContextMenu.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ContextMenu = () => {
+    return (
+        <div className="context-menu">
+            <div>Edit</div>
+            <div>Delete</div>
+        </div>
+    )
+}
+
+export default ContextMenu
